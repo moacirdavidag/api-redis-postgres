15271378dc337e0b03e7a1693594bd89c5390799
moacirdavidag <moacirdavidag@gmail.com>
2022-06-20 16:13:06 -0300

    Cache com Redis implementado

--- controllers/PessoaController.js
+++ controllers/PessoaController.js
@@ -1,82 +1,94 @@
 const { response } = require('express');
 const res = require('express/lib/response');
 const Pessoa = require('../models/pessoa');
-const redis = require('../database/redis');
+const client = require('../database/redis');
 
-const getPessoas = async (request, response)=>{
-    if(!request.params.email) {
+const getPessoas = async (request, response) => {
+
+    if (!request.params.email) {
         const pessoas = await Pessoa.findAll();
         response.status(200).send(pessoas);
     } else {
         const email = request.params.email;
-        const pessoa = await Pessoa.findOne({
-            where: {
-                email: email
+        const resultado = await client.get(email);
+        if(resultado !== null) {
+            const pessoa = JSON.parse(resultado);
+            response.status(200).send(pessoa);
+        } else {
+            const pessoa = await Pessoa.findOne({
+                where: {
+                    email: email
+                }
+            });
+            if (pessoa === null) {
+                response.status(200).send("Pessoa não encontrada!");
+            } else {
+                await client.set(email, JSON.stringify(pessoa), {
+                    EX: 3600
+                });
+                response.status(200).send(pessoa);
             }
-        });
-        if(pessoa === null) {
-            response.status(200).send("Pessoa não encontrada!");
+            
         }
-        response.status(200).send(pessoa);
+        
     }
-    
 };
 
-const addPessoa = async (request, response) =>{
+const addPessoa = async (request, response) => {
     const pessoa = Pessoa.build(request.body);
-    pessoa.save().then(()=>{
+    pessoa.save().then(() => {
         response.status(200).send('Usuário criado!');
-        redis.inserirPessoa(request.body.email, request.body);
-    }).catch(err =>{
+    }).catch(err => {
         response.status(400).send('Falha ao salvar');
     });
 
 };
 
-const deletarPessoa = async (request, response)=>{
+const deletarPessoa = async (request, response) => {
     const email = request.params.email;
 
     Pessoa.destroy({
         where: {
             email: email
         }
-    }).then(result=>{
-        if(result>0){
+    }).then(result => {
+        if (result > 0) {
             response.status(200).send('Usuário removido');
-        }else{
+        } else {
             response.status(200).send('Usuário não encontrado');
         }
-    }).catch(err=>{
+    }).catch(err => {
         response.status(400).send('Falha ao remover');
     });
 
 };
 
-const atualizarPessoa = async(request, response)=>{
-    
+const atualizarPessoa = async (request, response) => {
+
     Pessoa.update({
-        nome: request.body.nome},
-            {
-                where: {
-                    email: request.body.email
+        nome: request.body.nome
+    },
+        {
+            where: {
+                email: request.body.email
             }
         }
-    ).then(result=>{
-        if(result>0){
+    ).then(result => {
+        if (result > 0) {
             response.status(200).send('Usuário atualizado');
-        }else{
+        } else {
             response.status(200).send('Usuário não encontrado');
         }
-    }).catch(err=>{
+    }).catch(err => {
         console.log(err);
         response.status(400).send('Falha ao atualizar');
     });
 
 }
 
-const sincronizar = async(request, response) =>{
+const sincronizar = async (request, response) => {
     await Pessoa.sync();
     response.status(200).send('Sincronizado');
 }
 
-module.exports = {getPessoas, addPessoa, sincronizar, deletarPessoa, atualizarPessoa};
+module.exports = { getPessoas, addPessoa, sincronizar, deletarPessoa, atualizarPessoa };
--- database/redis.js
+++ database/redis.js
@@ -3,32 +3,11 @@ const redis = require('redis');
 const client = redis.createClient({
         url: "redis://localhost:6379"
     });
-    
-module.exports = {
-    
-    async inserirPessoa(pessoa) {
-    
-        await client.connect();
-        try {
-            const resultado = await client.set(pessoa.email, JSON.stringify(pessoa), {
-                EX: 3600
-            });
-            console.log(resultado);
-        } catch(e) {
-            console.log(`Error: ${e}`);
-        };
-    
-    },
-    
-    async recuperarPessoa(email) {
-        try {
-            await client.connect();
-            const resultado = await client.get(email);
-            return resultado;
-        } catch(e) {
-            console.log(`Error: ${e}`);
-        }
-    }
-}
+
+(async function conectar() {
+    await client.connect();
+})()
+
+module.exports = client;
 
 
